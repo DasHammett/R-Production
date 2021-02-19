422e0563cc2e0d11c401148061c3c88d6565e172
DasHammett <clockwork80@gmail.com>
2021-02-19 15:43:53 +0100

    Update 4sq Connection Map.R

--- 4sq Connection Map.R
+++ 4sq Connection Map.R
@@ -17,7 +17,7 @@ fqs <- select(fqs,3,4) %>%
    tibble::rowid_to_column() #rowid_to_column to create index on destination
 
 # Set origin for connections
-origin <- c(2.1951747749949106,41.41819546685941)
+origin <- c()
 
 # Create df with origin lon,lat and extend it to size of venue checkins
 origen <- data.frame(venue.location.lat=origin[2], venue.location.lng = origin[1], type = "origin")
