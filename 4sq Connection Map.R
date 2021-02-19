aa02e2aa2bfdb442f4e5728be45eafba1fbd36fc
DasHammett <clockwork80@gmail.com>
2021-02-19 15:43:35 +0100

    Added cities, reworked colors, added coord slice

--- 4sq Connection Map.R
+++ 4sq Connection Map.R
@@ -3,11 +3,18 @@ library(dplyr)
 library(sf)
 
 # Load world map
-world <- st_read(file.choose())
+world <- st_read("/home/hammett/src/R Scripts/World Geo/World_Countries.shp")
+additional <- read.csv("/home/hammett/locations.txt", sep = ",", header = FALSE) %>% rename(venue.location.lat = V1, venue.location.lng = V2, type = V3)
+
+# Load cities shapefile
+cities <- st_read("/home/hammett/src/R Scripts/World Geo/ne_10m_urban_areas.shp")
 
 # Load csv file with foursquare checkin venue location
-fqs <- read.csv(file.choose(), sep = ";", header = TRUE, encoding = "UTF-8")
-fqs <- select(fqs,3,4) %>% mutate(type = "destination") %>% tibble::rowid_to_column() #rowid_to_column to create index on destination
+fqs <- read.csv("/home/hammett/src/R Scripts/fq_checkins.csv", sep = ";", header = TRUE, encoding = "UTF-8")
+fqs <- select(fqs,3,4) %>% 
+   mutate(type = "destination") %>% 
+   bind_rows(additional) %>%
+   tibble::rowid_to_column() #rowid_to_column to create index on destination
 
 # Set origin for connections
 origin <- c(2.1951747749949106,41.41819546685941)
@@ -28,7 +35,9 @@ routes_lines <- routes %>%
 
 # Plot chart
 ggplot() +
-  geom_sf(data = world, fill = "#2a2a2a", colour = "#8E8E8E", size = 0.05) +
-  geom_sf(data = routes_lines, colour = "yellow", size = 0.1, alpha = 0.5) +
+  geom_sf(data= world, fill = "#2a2a2a", colour = "#8E8E8E", size = 0.05) +
+  geom_sf(data = cities, colour = alpha("#FEF7C3",0.1)) +  
+  geom_sf(data = routes_lines, colour = "#E5FF99", size = 0.15, alpha = 0.5) +
+  #coord_sf(xlim = c(-10,150), ylim = c(0,80)) + 
   theme_void() +
-  theme(panel.background = element_rect(fill = "#261d11"))
+  theme(panel.background = element_rect(fill = "#001328")) #"#261d11"))
