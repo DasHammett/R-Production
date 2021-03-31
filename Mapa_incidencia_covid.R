82ca3caca1c75c6a265a1bed4c891783a2817316
DasHammett <clockwork80@gmail.com>
2021-03-31 18:06:33 +0200

    Changed direction and incidence to 7 days

--- Mapa_incidencia_covid.R
+++ Mapa_incidencia_covid.R
@@ -17,6 +17,7 @@ raw$TipusCasData <- dmy(raw$TipusCasData)
 raw$ComarcaDescripcio <- trimws(raw$ComarcaDescripcio,whitespace=" ")
 raw$MunicipiCodi <- stringr::str_pad(raw$MunicipiCodi, 5, side = "left", pad = 0)
 raw$MunicipiDescripcio <- str_to_title(raw$MunicipiDescripcio)
+max_data <- ymd(max(raw$TipusCasData) - 1)
 
 pob <- read.csv("https://analisi.transparenciacatalunya.cat/api/views/b4rr-d25b/rows.csv?accessType=DOWNLOAD&sorting=true",
                 header = TRUE,
@@ -28,7 +29,6 @@ pob$CodiINE <- gsub(".{1}$", "", pob$Codi)
 pob$CodiINE <- stringr::str_pad(pob$CodiINE, 5, side = "left", pad = 0)
 pob_total <- pob %>% filter(Literal == "Catalunya") %>% select(Cens) %>% pull()
 
-max_data <- ymd(max(raw$TipusCasData) - 1)
 resum <- raw %>%
    filter(TipusCasDescripcio != "Sospitós", TipusCasData <= max_data) %>%
    group_by(TipusCasData, MunicipiDescripcio, MunicipiCodi) %>%
@@ -39,10 +39,7 @@ resum <- raw %>%
    arrange(MunicipiDescripcio) %>% 
    complete(TipusCasData = full_seq(c(min(TipusCasData),max_data), period = 1), fill = list(NumCasos = 0)) %>%
    mutate(NAcumulats = roll_sum(NumCasos,n = 14, align = "right", fill = NA),
-          Direccio = case_when(NAcumulats -lag(NAcumulats) > 0 ~ "▲" , 
-                               NAcumulats - lag(NAcumulats) < 0 ~ "▼", 
-                               NAcumulats - lag(NAcumulats) == 0 ~ "▷", 
-                               TRUE ~ "→")) %>%
+          diff = paste0("(" ,sprintf("%+d",NAcumulats-lag(NAcumulats,7)),")")) %>%
    fill(MunicipiCodi) 
 
 evolucio <- resum %>% 
@@ -55,7 +52,7 @@ evolucio <- resum %>%
             panel.grid.minor = element_blank(),
             plot.title = element_text(size = 9, hjust = 1,face = "italic")) +
       labs(title = "Evolució incidència a Catalunya") +
-      scale_x_date(date_breaks = "1 month", labels = label_date_short())
+      scale_x_date(date_breaks = "2 month", labels = label_date_short())
 
 mapa_N <- left_join(mapa, filter(resum, TipusCasData == max_data), by = c("CODIINE" = "MunicipiCodi"))
 mapa_N <- left_join(mapa_N, pob[,c("CodiINE","Cens")], by = c("CODIINE" = "CodiINE"))
@@ -71,7 +68,7 @@ mapa_N <- mapa_N %>%
 	  )
 
 Incidencia <- round(sum(mapa_N$NAcumulats, na.rm = T)/pob_total*1e5,0)
-Incidencia_dia_anterior <- resum %>% ungroup() %>% filter(TipusCasData == max_data-1) %>% summarise(N = round(sum(NAcumulats, na.rm = TRUE)/pob_total*1e5,0)) %>% pull()
+Incidencia_dia_anterior <- resum %>% ungroup() %>% filter(TipusCasData == max_data-7) %>% summarise(N = round(sum(NAcumulats, na.rm = TRUE)/pob_total*1e5,0)) %>% pull()
 #min_green <- rescale(100,from=range(mapa_N$Incidencia, na.rm = TRUE))
 #min_yellow <- rescale(300,from=range(mapa_N$Incidencia, na.rm = TRUE))
 #min_red <- rescale(1000,from=range(mapa_N$Incidencia, na.rm = TRUE))
@@ -89,7 +86,7 @@ ggplot(mapa_N) +
    theme_void() +
    labs(fill = "Incidencia",
         title = "Incidència COVID-19 últims 14 dies per cada 100.000 habitants",
-        subtitle = paste0("Incidència a Catalunya: ",Incidencia, " (" ,sprintf("%+d",Incidencia-Incidencia_dia_anterior)," vs dia anterior",")")) +
+        subtitle = paste0("Incidència a Catalunya: ",Incidencia, " (" ,sprintf("%+d",Incidencia-Incidencia_dia_anterior)," vs setmana anterior",")")) +
    theme(plot.title = element_text(family="Helvetica Neue LT Std", size = 14),
          plot.subtitle = element_text(family="Helvetica Neue LT Std", size = 12,face = "italic")) +
    annotation_custom(
@@ -98,9 +95,9 @@ ggplot(mapa_N) +
             as.data.frame() %>% 
             arrange(desc(Cens)) %>% 
             slice(1:7) %>% 
-            select(NOM_MUNI,NAcumulats,Cens,Incidencia,Nivell,Direccio) %>%
-            mutate(Incidencia = paste(round(Incidencia),Direccio,sep = " ")) %>%
-            select(-Direccio) %>%
+            select(NOM_MUNI,NAcumulats,Cens,Incidencia,Nivell,diff) %>%
+            mutate(NAcumulats = paste(NAcumulats,diff,sep = " "), Incidencia = round(Incidencia)) %>%
+            select(-diff) %>%
             rename(Municipi=NOM_MUNI,Confirmats=NAcumulats,Incidència = Incidencia),
 	  rows = NULL,
 	  theme = ttheme_minimal(core=list(bg_params=list(fill=c("#E9E9E9","white")),fg_params=list(cex = 0.8)))
@@ -115,3 +112,4 @@ ggplot(mapa_N) +
     ymin = 4778000,
     ymax = 4705000
     )
+
