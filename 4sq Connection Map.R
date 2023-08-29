6c47c3d5807004e0179c57e041ae9a2b19b9cef1
DasHammett <clockwork80@gmail.com>
2023-08-29 07:54:30 +0200

    Ocean and manual graticule for wintri projection

--- 4sq Connection Map.R
+++ 4sq Connection Map.R
@@ -35,6 +35,96 @@ routes_lines <- routes %>%
   st_cast("LINESTRING") %>%
   st_segmentize(units::set_units(20,km))
 
+# General projection
+crs <- "+proj=fahey"
+lats <- c(90:-90, -90:90, 90)
+longs <- c(rep(c(180, -180), each = 181), 180)
+
+# turn into correctly projected sf collection
+ocean <- 
+  list(cbind(longs, lats)) %>%
+  st_polygon() %>%
+  st_sfc( # create sf geometry list column
+    crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+  ) %>% 
+  st_sf() %>%
+  st_transform_proj(crs = crs)
+
+grat <- 
+  st_graticule(lat = c(-89.9, seq(-80, 80, 20), 89.9)) %>%
+  st_transform_proj(crs = crs)
+  
+# Plot chart
+# list of available projections: https://proj.org/en/9.2/operations/projections
+ggplot() +
+  geom_sf(data = ocean, fill = "#03101E", colour = NA) + 
+  geom_sf(data = grat, colour = "grey40", linewidth = 0.1) +
+  geom_sf(data = world, fill = "#2a2a2a", colour = "#8E8E8E", linewidth = 0.05) +
+  geom_sf(data = cities, colour = NA ) +
+  geom_sf(data = routes_lines,  colour = "#E5FF99", linewidth = 0.15, alpha = 0.50, lineend = "round") +
+  coord_sf(crs = st_crs(crs)) +
+  #scale_x_continuous(breaks = seq(-180,180,20), limits = c(-185e5,185e5)) +
+  #scale_y_continuous(breaks = seq(-100,100,10), limits = c(-100e5,100e5)) +
+  theme(panel.background = element_blank()) +
+  theme_void()
+
+
+# Wintri Tipel projection
+crs <- "+proj=wintri +datum=WGS84 +no_defs +over"
+lats <- c(90:-90, -90:90, 90)
+longs <- c(rep(c(180, -180), each = 181), 180)
+
+ocean <- 
+  list(cbind(longs, lats)) %>%
+  st_polygon() %>%
+  st_sfc( # create sf geometry list column
+    crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+  ) %>% 
+  st_sf() %>%
+  st_transform_proj(crs = crs)
+
+grat <- 
+  st_graticule(lat = c(-89.9, seq(-80, 80, 20), 89.9)) %>%
+  st_transform_proj(crs = crs)
+
+# Plot chart
+# list of available projections: https://proj.org/en/9.2/operations/projections
+ggplot() +
+  geom_sf(data = ocean, fill = "#03101E", colour = NA) + 
+  geom_sf(data = grat, colour = "grey50", linewidth = 0.1) +
+  geom_sf(data = st_transform_proj(world, crs = crs), fill = "#2a2a2a", colour = "#8E8E8E", linewidth = 0.05) +
+  geom_sf(data = st_transform_proj(cities, crs = crs), colour = NA ) +
+  geom_sf(data = st_transform_proj(routes_lines, crs = crs), colour = "#E5FF99", linewidth = 0.15, alpha = 0.50, lineend = "round") +
+  coord_sf(datum = NULL) +
+  #scale_x_continuous(breaks = seq(-180,180,20), limits = c(-185e5,185e5)) +
+  #scale_y_continuous(breaks = seq(-100,100,10), limits = c(-100e5,100e5)) +
+  theme(panel.background = element_blank()) +
+  theme_void()
+
+
+
+### Barcelona ###
+bcn <- st_read("C://Users//Jordi Vidal//Desktop//R//Catalunya GEO//terme-municipal.geojson")
+
+fqs <- read.csv("C://Users//Jordi Vidal//Desktop//R//fq_checkins.csv", sep = ";", header = TRUE, encoding = "UTF-8")
+fqs <- 
+  fqs %>%
+  filter(venue.location.city == "Barcelona", venue.location.lng < 2.4, venue.location.lat < 41.5) %>%
+  select(2,3,4) %>%
+  group_by(venue.location.lat, venue.location.lng, venue.name) %>%
+  summarise(n = n(), .groups = "drop" ) %>%
+  arrange(-n)
+
+library(ggrepel)
+ggplot() +
+  geom_sf(data = bcn) +
+  geom_sf(data = st_set_crs(st_as_sf(fqs, coords = c("venue.location.lng", "venue.location.lat")), 4326), fill = "black", shape = 16, aes(alpha = n), size = 0.1) +
+  geom_text_repel(data = top_n(fqs,10), aes(label = paste0(venue.name, "(",n,")"), y = venue.location.lat, x = venue.location.lng), size = 1, min.segment.length = 0, segment.size = 0.1, arrow = arrow(length = unit(0.002, "npc")), point.padding = 0)+
+  theme_void() +
+  scale_alpha(range = c(0.1,1)) +
+  theme(legend.position = "none")
+
+### Old plot ###
 # Plot chart
 # list of available projections: https://proj.org/en/9.2/operations/projections
 ggplot() +
